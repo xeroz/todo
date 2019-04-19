9b846e45e43e3242adbf669aadb031f00ddab9df
juan <chavezvasquezjuan@gmail.com>
2019-04-19 11:21:21 -0500

    Incidence serializer

--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
-from .models import Project, Priority
+from .models import Project, Priority, Incidence
 
 
 class ProjectSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
+    slug = serializers.SlugField()
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
@@ -21,6 +22,20 @@ class PrioritySerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Priority.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+class IncidenceSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    slug = serializers.SlugField()
+
+    def create(self, validated_data):
+        return Incidence.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
