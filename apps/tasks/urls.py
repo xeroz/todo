36c68cf16ace602b6f3b0f968fecb7f5682d9bee
juan <chavezvasquezjuan@gmail.com>
2019-05-24 23:25:13 -0500

    urls

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from .views import (
     ProjectViewSet, PriorityList, PriorityDetail,
-    IncidenceViewSet, SprintViewSet)
+    IncidenceViewSet, SprintViewSet, StatusTaskViewSet,
+    TaskViewSet)
 from rest_framework.routers import DefaultRouter
 
 app_name = 'tasks'
@@ -11,6 +12,8 @@ router = DefaultRouter()
 router.register('projects', ProjectViewSet)
 router.register('incidents', IncidenceViewSet)
 router.register('sprints', SprintViewSet)
+router.register('status-task', StatusTaskViewSet)
+router.register('tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
